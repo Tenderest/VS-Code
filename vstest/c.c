628c4473a69437d833dd9a986be2bf0a20a2bee6
Tenderest <2087886585@qq.com>
2019-12-08 10:08:20 +0800

    2019/12/08 Windows上的VS Code配置

---
+++ vstest/c.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+    int a = 0, b = 0;
+    printf("Hello World!\n");
+    for (int i = 0; i < 5; i++)
+    {
+        a ++;
+        b ++;
+    }
+    printf("a= %d, b = %d\n", a, b);
+    system("pause");
+    return 0;
+}
